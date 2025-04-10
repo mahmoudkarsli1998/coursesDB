b003a0ee33f521618978fe69c7711cde2cccf462
Mahmoud Karsli <mahmoudkarsli1998@outlook.com>
2025-04-10 10:36:01 +0200

    enhance async wrapper and app error middlewares

--- middleware/asyncwrapper.js
+++ middleware/asyncwrapper.js
@@ -1,8 +1,18 @@
-const { error } = require("console");
+// const { error } = require("console");
 
-module.exports = (asyncFn)=>{
-    return (req,res,next)=>{
-        asyncFn(req,res,next).catch((error)=>{
+// module.exports = (asyncFn)=>{
+//     return (req,res,next)=>{
+//         asyncFn(req,res,next).catch((error)=>{
+//             next(error);
+//         });
+//     };
+// }
+const { error } = require("console"); // You can remove this line as it's not used
+const AppError = require('../utils/appError');
+
+module.exports = (asyncFn) => {
+    return (req, res, next) => {
+        asyncFn(req, res, next).catch((error) => {
             next(error);
         });
     };
--- utils/appError.js
+++ utils/appError.js
@@ -1,14 +1,17 @@
-class AppError extends Error{
-    constructor(){
-        super();
+class AppError extends Error {
+    constructor(message, statusCode, statusText) {
+      super(message); // Initialize the parent Error with the message
+      this.statusCode = statusCode;
+      this.statusText = statusText;
+      // Optionally capture the stack trace (excluding the constructor)
+      Error.captureStackTrace(this, this.constructor);
     }
-    create(message, statusCode , statusText){
-        this.message = message;
-        this.statusCode = statusCode;
-        this.statusText = statusText;
-        return this;
+  
+    // Static create function returns a new instance of AppError
+    static create(message, statusCode, statusText) {
+      return new AppError(message, statusCode, statusText);
     }
-
-}
-
-module.exports = new AppError();
+  }
+  
+  module.exports = AppError;
+  
