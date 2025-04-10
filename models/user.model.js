b336210dac05d8dae66842d35167e9329e8e3f90
Mahmoud Karsli <mahmoudkarsli1998@outlook.com>
2025-04-10 10:55:39 +0200

    Create Users Model

---
+++ models/user.model.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+require('dotenv').config();
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        validate:[validator.isEmail,'Please enter a valid email address'],
+    },
+    password:{
+        type:String,
+        required:true,
+        minlength:[6,'Password must be at least 6 characters'],
+    },
+    role:{
+        type:String,
+        enum:['admin','user'],
+        default:'user',
+        
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now()
+    }
+});
+module.exports = mongoose.model('User',userSchema);
